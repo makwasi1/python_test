b3d317c5d5ff0e5632cfae740a6d41e321d0e0fe
makwasi1 <crismakwasi12@gmail.com>
2023-03-30 07:43:43 +0300

    initiall commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# as the code grows you can put all the tests into a single folder
+# its convetion test_*.py for all test files
+# how to run with verbose flag python -m unittest -v test
+
+import unittest
+
+from my_sum import sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
